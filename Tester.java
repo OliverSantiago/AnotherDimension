3afd44d5fde0aae45f47497a234906bae8bf04cf
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-10-22 21:17:02 -0400

    first method works

---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester{
+public static void main(String[] args) {
+  //System.out.println(Triangle.distance(0.0,0.0,3.0,60.0));
+  //System.out.println(Triangle.classify(0.0,0.0,3.0,0.0,1.5,3.0));
+  //System.out.println(Triangle.perimeter(0.0,3.0,70.0,60.0,1.0,80.0));
+  //System.out.println(Triangle.area(0.0,3.0,70.0,60.0,1.0,80.0));
+  int[]arr=new int[]{0,4,5};
+  System.out.println(ArrayOps.sum(arr));
+}
+}
