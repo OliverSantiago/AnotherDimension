825cd6aef9cb47f24605d2e76b466cb65aa59862
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-10-22 22:14:35 -0400

    Method 5

--- Tester.java
+++ Tester.java
@@ -5,10 +5,11 @@ public static void main(String[] args) {
   //System.out.println(Triangle.perimeter(0.0,3.0,70.0,60.0,1.0,80.0));
   //System.out.println(Triangle.area(0.0,3.0,70.0,60.0,1.0,80.0));
   int[]arr=new int[]{0,4,5};
-  int[][]arr2={{1,3,5},{4,7,5}};
+  int[][]arr2={{1,3,5},{4,7,5,8}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
   //System.out.println(ArrayOps.sumRows(arr2));
-  System.out.println(ArrayOps.largestInRows(arr2));
+  //System.out.println(ArrayOps.largestInRows(arr2));
+  System.out.println(ArrayOps.sum(arr2));
 }
 }
