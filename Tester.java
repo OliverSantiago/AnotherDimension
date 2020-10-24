8420a347de0f00d529f85b52d083d71c3fc456c4
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-10-24 17:55:12 -0400

    Tests

--- Tester.java
+++ Tester.java
@@ -1,15 +1,16 @@
+import java.util.Arrays;
 public class Tester{
 public static void main(String[] args) {
   //System.out.println(Triangle.distance(0.0,0.0,3.0,60.0));
   //System.out.println(Triangle.classify(0.0,0.0,3.0,0.0,1.5,3.0));
   //System.out.println(Triangle.perimeter(0.0,3.0,70.0,60.0,1.0,80.0));
   //System.out.println(Triangle.area(0.0,3.0,70.0,60.0,1.0,80.0));
-  int[]arr=new int[]{0,4,5};
-  int[][]arr2={{1,3,5},{4,7,5,8}};
-  //System.out.println(ArrayOps.sum(arr));
-  //System.out.println(ArrayOps.largest(arr));
-  //System.out.println(ArrayOps.sumRows(arr2));
-  //System.out.println(ArrayOps.largestInRows(arr2));
-  System.out.println(ArrayOps.sum(arr2));
+  int[][]A={{1,0,12,-1},{7,-2,2,1},{-5,-2,2,-9}};
+  int[]B={1,3,5};
+  System.out.println(ArrayOps.sum(B));
+  System.out.println(ArrayOps.largest(B));
+  System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
+  System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
+  System.out.println(ArrayOps.sum(A));
 }
 }
