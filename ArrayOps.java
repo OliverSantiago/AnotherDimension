f98108e35b960112d60bab66203572240cb53f99
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-10-27 15:11:36 -0400

    Bug Corrections

--- ArrayOps.java
+++ ArrayOps.java
@@ -72,10 +72,18 @@ public class ArrayOps{
   }
 
   public static boolean isColMagic(int[][] matrix){
-    int temp = 0;
-    temp = helpcolumnsum(matrix,0);
-    for (int i = 0; i < matrix[i].length; i++){
-      if(temp!=helpcolumnsum(matrix,i)){
+    //int temp = 0;
+    //temp = helpcolumnsum(matrix,0);
+    //for (int i = 0; i < matrix[i].length; i++){
+    //  if(temp!=helpcolumnsum(matrix,i)){
+    //    return false;
+    //  }
+    //}
+    //return true;
+    int[]answer = sumCols(matrix);
+    int test = answer[0];
+    for (int i = 0; i < answer.length; i++){
+      if (answer[0]!=test){
         return false;
       }
     }
